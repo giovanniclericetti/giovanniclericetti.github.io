80cbce94af2e3a8b2aed5e96cac3c7e8a65f7717
Giovanni Clericetti <giovanni.clericetti@gmail.com>
2022-11-03 11:55:14 +0100

    Nuxt Links

--- components/About-Header.vue
+++ components/About-Header.vue
@@ -4,8 +4,8 @@
 
 
     <b-row class="p-0 m-0" align-v="center">
-      <b-col cols="6" class="p-0 gradient">  <a href="index"> <b-icon icon="arrow-left-circle-fill" style="width: 30px; height: 30px;"></b-icon> </a> </b-col>
-      <b-col cols="6" class="p-0 text-right"> <a href="about" > <h5 class="m-0 " > About Me </h5> </a> </b-col>
+      <b-col cols="6" class="p-0 gradient">  <NuxtLink to="/"> <b-icon icon="arrow-left-circle-fill" style="width: 30px; height: 30px;"></b-icon> </NuxtLink> </b-col>
+      <b-col cols="6" class="p-0 text-right"> <NuxtLink to="about"> <h5 class="m-0 " > About Me </h5> </NuxtLink> </b-col>
     </b-row>
 
 
@@ -15,7 +15,7 @@
 
 <script>
 export default {
-  name: "G-Footer"
+  name: "GFooter"
 }
 </script>
 
