975f57c6b7653df48c5e377c4a27c42cdbd02ee3
Giovanni Clericetti <giovanni.clericetti@gmail.com>
2024-03-04 18:27:54 +0100

    vercel

--- plugins/vue-prlx.js
+++ plugins/vue-prlx.js
@@ -2,14 +2,7 @@
 
 import Vue from 'vue';
 import VuePrlx from 'vue-prlx';
-import VueLazyload from 'vue-lazyload';
-import VueProgressiveImage from 'vue-progressive-image';
+
 
 Vue.use(VuePrlx);
-Vue.use(VueLazyload, {
-  preLoad: 1.3,
-  error: 'path/to/error.png',
-  loading: 'path/to/loading.gif',
-  attempt: 1
-});
-Vue.use(VueProgressiveImage);
+
