8a057a324eb3fc3f1fa9022e2f5203b8e6737791
Giovanni Clericetti <giovanni.clericetti@gmail.com>
2024-04-18 18:29:48 +0200

    It-Back

--- pages/it-back.vue
+++ pages/it-back.vue
@@ -25,18 +25,9 @@
 
       <ProjectDescription
         description="
-It is precisely from the comeback of outdated, vintage products
-IT-BACK was born, characterized by a minimal, modern and decidedly colorful style,
-coupled with equally direct and lively communication.
-The experience starts with the Instagram profile, where the user
-is intrigued by social content that shows some products in their details
-without ever explicitly revealing what they are about, but only hinting that
-they are great technological innovations. It continues with the website, where this
-path of curiosity and recognition continues, growing the hype. The slide scrolling of
-the site helps make this step-by-step journey more immersive. Moving on, one will discover
-that these products, presented as extremely innovative, are instead now disused because they are obsolete.
-But the experience does not stop there, as they are reevaluated since they carry with them a new meaning
-that makes those who use them unique and special."
+IT-BACK was born from the comeback of obsolete, vintage products, characterised by a minimalist, modern and decidedly colourful style, coupled with an equally direct and lively communication. The focus of the project is on the user's experience throughout the entire journey.
+he experience starts with the Instagram profile, where the user is fascinated by social content that shows some products in detail, without ever explicitly revealing what they are, but only suggesting that they are great technological innovations.
+"
         team="B. Barzaghi, E. Ceriani, B. Duina, F. Nozza, E. Peverelli"
         role="Brand Identity, UX-UI, Art Direction, Keynote Presentation, Social Media, Coding (Wordpress)"
         delivery="Software and Graphics Lab, 2022"
@@ -44,7 +35,7 @@ that makes those who use them unique and special."
       >
 
         <template #link>
-          <div class="mt-5">
+          <div class="mt-4">
             <a href="https://itback.altervista.org/" target="_blank">
               <ProjectLink textlink="Website"></ProjectLink>
             </a>
@@ -59,61 +50,91 @@ that makes those who use them unique and special."
 
       <b-row class="mt-4">
 
+        <b-col cols="12" md="12" class=" py-md-3 py-2 px-3 d-md-block d-none">
+          <div class="imgbox">
+            <img class="img-parallax imgproject h500 small-dis" style="
+     background-image: url(/images/it-back/itback6.jpg)"
+                 v-prlx.background="{ speed: 0.1 }"></div>
+        </b-col>
+
+        <b-col cols="12" md="12" class=" py-md-3 py-2 px-3 d-block d-md-none">
+          <div class="imgbox">
+            <img class="img-parallax imgproject h500 small-dis" style="
+     background-image: url(/images/it-back/itback6-sm.jpg)"
+                 v-prlx.background="{ speed: 0.1 }"></div>
+        </b-col>
+
         <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
 
           <div class="imgbox">
             <img class="img-parallax imgproject h600" style="
-     background-image: url(/images/it-back/itback1.svg)"
+     background-image: url(/images/it-back/itback1.jpg)"
                  v-prlx.background="{ speed: 0.2 }"></div>
         </b-col>
+
+        <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
+
+          <ProjectDescription2
+
+            title="Platform User Experience"
+
+            description="
+The experience continues with the website, where this path of curiosity and recognition is continued and the hype grows. The site's scrolling slides help to make this step-by-step journey even more immersive. As the journey continues, the visitor discovers that these products, presented as highly innovative, are in fact being phased out as they become obsolete. But the experience does not stop there, as they are reassessed as they take on a new meaning that makes those who use them unique and special."
+
+          /> </b-col>
 
         <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
         <div class="imgbox">
           <img class="img-parallax imgproject h600" style="
-     background-image: url(/images/it-back/itback2.svg)"
+     background-image: url(/images/it-back/itback2.jpg)"
                v-prlx.background="{ speed: 0.2 }"></div>
         </b-col>
 
         <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
-          <div class="imgbox">
-            <img class="img-parallax imgproject h400" style="
-     background-image: url(/images/it-back/itback3.svg)"
-                 v-prlx.background="{ speed: 0.1 }"></div>
-        </b-col>
 
-        <b-col cols="12" md="6" class=" py-md-3 py-2 px-3">
+          <ProjectDescription2
 
-          <div class="imgbox">
-            <img class="img-parallax imgproject h500" style="
-     background-image: url(/images/it-back/itback4.svg)"
-                 v-prlx.background="{ speed: 0.2 }"></div>
-        </b-col>
+            title="Content and Sections"
 
-        <b-col cols="12" md="6" class=" py-md-3 py-2 px-3">
-          <div class="imgbox">
-            <img class="img-parallax imgproject h500" style="
-     background-image: url(/images/it-back/itback5.svg)"
-                 v-prlx.background="{ speed: 0.2 }"></div>
-        </b-col>
+            description="
+The objects presented on the web platform are four, like the sections of the site: Walkman, Tamagotchi, Walkie-Talkie and Floppy Disk. Each object is associated with a colour and a specific narrative path."
+          /> </b-col>
 
         <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
           <div class="imgbox">
-            <img class="img-parallax imgproject h500 small-dis" style="
-     background-image: url(/images/it-back/itback6.svg)"
+            <img class="img-parallax imgproject h400" style="
+     background-image: url(/images/it-back/itback3.jpg)"
                  v-prlx.background="{ speed: 0.1 }"></div>
+        </b-col>
+
+
+
+        <b-col cols="12" md="6" class=" py-md-3 py-2 px-3">
+
+          <div class="imgbox">
+            <img class="img-parallax imgproject h500" style="
+     background-image: url(/images/it-back/itback4.jpg)"
+                 v-prlx.background="{ speed: 0.2 }"></div>
+        </b-col>
+
+        <b-col cols="12" md="6" class=" py-md-3 py-2 px-3">
+          <div class="imgbox">
+            <img class="img-parallax imgproject h500" style="
+     background-image: url(/images/it-back/itback5.jpg)"
+                 v-prlx.background="{ speed: 0.2 }"></div>
         </b-col>
 
         <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
           <div class="imgbox">
             <img class="img-parallax imgproject h600" style="
-     background-image: url(/images/it-back/itback8.svg)"
+     background-image: url(/images/it-back/itback8.jpg)"
                  v-prlx.background="{ speed: 0.1 }"></div>
         </b-col>
 
         <b-col cols="12" md="12" class=" py-md-3 py-2 px-3">
           <div class="imgbox">
             <img class="img-parallax imgproject h400" style="
-     background-image: url(/images/it-back/itback7.svg)"
+     background-image: url(/images/it-back/itback7.jpg)"
                  v-prlx.background="{ speed: 0.2 }"></div>
         </b-col>
 
